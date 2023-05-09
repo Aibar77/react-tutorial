3c083ee03b605e3122f4bcdf43c511e03c9c59a0
Mambet Aibar <109662937+Aibar77@users.noreply.github.com>
2023-05-09 19:35:17 +0600

    Basic 1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const el = (
-  <div>
-    <h1>My ToDo List</h1>
-    <input placeholder="search" />
+const TodoList = () => {
+  const items = ["Learn React", "Build App"];
+  return (
     <ul>
-      <li>Learn React</li>
-      <li>Drink Tea</li>
+      <li>{items[0]}</li>
+      <li>{items[1]}</li>
     </ul>
-  </div>
-);
+  );
+};
+const AppHeader = () => {
+  return <h1>My ToDo List</h1>;
+};
+
+const SearchPanel = () => {
+  const searchText = "Type here to search";
+  const searchStyle = {
+    fontSize: 20,
+    padding: 5,
+  };
+  return <input style={searchStyle} placeholder={searchText} />;
+};
+
+const App = () => {
+  return (
+    <div>
+      <AppHeader />
+      <SearchPanel />
+      <TodoList />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(el);
+root.render(<App />);
